7c356f365d4654a7642c0b1bc0025f1ed15d457e
dhalpern <David@172-17-60-239.DYNAPOOL.NYU.EDU>
2015-08-07 18:32:20 -0400

    reorg and monitormode

--- Code/mln/sigma_deriv.m
+++
@@ -1,7 +0,0 @@
-function s = sigma_deriv(h, ftype)
-
-if strcmp(ftype,'tanh')
-    s = 1-tanh(h).^2;
-elseif strcmp(ftype,'relu')
-    s = heaviside(h);
-end
--- Code/mln/sigma_func.m
+++
@@ -1,7 +0,0 @@
-function s = sigma_func(h,ftype)
-
-if strcmp(ftype,'tanh')
-    s = tanh(h);
-elseif strcmp(ftype,'relu')
-    s = max(0,h);
-end
